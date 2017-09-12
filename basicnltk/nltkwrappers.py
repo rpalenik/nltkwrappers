9ff07779cde4bd432fcbc186310b23f6999a29a5
rpale <rpalenik2@gmail.com>
2017-09-12 17:48:15 +0200

    5th attempt

--- basicnltk/nltkwrappers.py
+++ basicnltk/nltkwrappers.py
@@ -2,9 +2,9 @@ class Tokenizer(object):
 
 
     def tokenize(self, input_bag, item):
-        import nltk, json
+        import nltk
         if item=='None':
-            self.output_bag = input_bag.map(nltk.word_tokenize, input_bag)
+            self.output_bag = input_bag.map(nltk.word_tokenize)
         else:
-            self.output_bag = input_bag.pluck(item).map(nltk.word_tokenize, input_bag)
+            self.output_bag = input_bag.pluck(item).map(nltk.word_tokenize)
         return self.output_bag
